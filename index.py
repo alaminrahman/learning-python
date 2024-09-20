224d92d651544a042b92c2369aee3f39b6def3f1
alaminrahman <sumonrahman3600@gmail.com>
2024-09-20 11:37:10 +0600

    Practicing list

---
+++ index.py
@@ -0,0 +1,60 @@
+# Concatenate two lists
+
+# items1 = ["apple", "banana", "cherry"] 
+# tupleitems = (1, 5, 7, 9, 3);
+# items1.extend(tupleitems)
+
+# print(items1)
+
+# items1 = ["apple", "banana", "cherry"]
+# items2 = [1, 5, 7, 9, 3] 
+
+# items1.extend(items2)
+# items = items1 + items2
+# print(items1)
+
+
+
+#add items
+# thislist = ["apple", "banana", "cherry"];
+# thislist.insert(2, "jackfruit")
+# thislist.append("jackfruit")
+# print(thislist)
+
+
+#change item value
+
+# thislist = ["apple", "banana", "cherry"]
+# thislist[1:2] = ["blackcurrant", "watermelon"]
+# print(thislist)
+
+# thislist = ["apple", "banana", "cherry", "orange", "kiwi", "mango"]
+# thislist[1:3] = ["blackcurrant", "watermelon"]
+# print(thislist)
+
+# fruits = ["apple", "banana", "cherry"]
+# fruits[1] = "blackcurrant"
+# print(fruits[1])
+
+# fruits = ["apple", "banana", "cherry"]
+# print(fruits[-1])
+
+# fruits = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+# print(fruits[2:5])
+
+
+
+# thislist = ["apple", "banana", "cherry"]
+# if 'banana' in thislist: 
+#     print("Yes, 'banana' is in the fruits list")
+# else:
+#     print('No, banana is not in the fruits list')
+
+
+# fruits = ["apple", "banana", "cherry"]
+# print(len(fruits))
+
+# x = 5
+# y = "John"
+
+# print(y)
